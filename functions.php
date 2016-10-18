648acf16539aca3fe436b70d69396a3d67cecbe4
Thomas Cober <tcober5@gmail.com>
2016-10-18 00:12:34 -0700

    Attempting to fix soundcloud js error and minor refactors

--- functions.php
+++ functions.php
@@ -1,13 +1,22 @@
 <?php
 
 function portfolio_scripts() {
-  
+
   wp_enqueue_style( 'siteStyle', get_template_directory_uri() . '/style.css', array(), '1.1' );
-  
-  wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js', array(), '1.0', true );
-  wp_enqueue_script( 'siteScript', get_template_directory_uri() . '/js/portfolio.js', array('jquery'), '1.0', true );
-  
+
+  wp_deregister_script('jquery');
+  wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js', array(), '1.0', true );
+  wp_enqueue_script('jquery');
+
+  wp_enqueue_script( 'siteScript', get_template_directory_uri() . '/site.min.js', array('jquery'), '1.0', true );
+
 }
 
 add_action( 'wp_enqueue_scripts', 'portfolio_scripts' );
+
+function add_oembed_soundcloud(){
+  wp_oembed_add_provider( 'http://soundcloud.com/*', 'http://soundcloud.com/oembed' );
+}
+add_action('init','add_oembed_soundcloud');
+
 ?>
--- home.php
+++ home.php
@@ -4,27 +4,27 @@
       <!-- SVG CODE BELOW IS STORED IN A SEPARATE PHP FILE IN A VARIABLE! -->
       <?php echo $svgcode; ?>
     </div>
-    
+
     <div class="container">
-      
+
       <div class="name">
         <a href="mailto:tcober5@gmail.com">
           <img src="<?php echo get_bloginfo('template_url'); ?>/images/mail.svg">
         </a>
         <h1>THOMAS&nbsp;COBER</h1>
       </div>
-       
+
       <div class="contentHolder">
         <!-- THIS IS WHERE THE IMAGES AND DECRIPTION LOAD TO -->
       </div>
-            
+
       <div class="player">
         <div class="musicCloser">
           <p>X</p>
         </div>
-        <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/12351544&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false"></iframe>
+        <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/12351544&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=false&amp;show_reposts=false"></iframe>
       </div>
-      
+
       <div class="twitter">
         <div class="twittersize">
           <p>
@@ -32,9 +32,9 @@
           </p>
         </div>
       </div>
-      
+
       <div class="worklist">
-        
+
         <ul>
           <li class="loader" data="getlytics-com">Getlytics.com</li>
           <li class="loader" data="lytics-landing">Lytics Landing Page</li>
@@ -47,25 +47,25 @@
           <li class="loader" data="library-box">Library Info Arcade</li>
           <li class="loader" data="adx-projects">ADX Projects</li>
         </ul>
-        
+
       </div>
-      
+
       <div class="menu">
-        
+
         <div class="design button">
           <h2>WORK</h2>
         </div>
-        
+
         <div class="music button">
           <h2>MUSIC</h2>
         </div>
-                 
+
         <a href="https://github.com/tcober" target="_blank">
           <div class="code button">
             <h2>CODE</h2>
           </div>
         </a>
-        
+
       </div>
       <!-- END OF CONTAINER DIV -->
     </div>
