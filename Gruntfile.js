42b56d105a03a82d0fe8c465d1ab35d18e1d3020
Thomas Cober <tcober5@gmail.com>
2016-10-17 18:23:22 -0700

    Setting up gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+  	pkg: grunt.file.readJSON('package.json'),
+  	
+		jshint: {
+			all: ['js/*.js']
+		},
+		
+		uglify: {
+  		options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'site.min.js': ['js/*.js']
+        }
+      }
+    },
+    
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'style.css': 'sass/style.scss'
+				}
+			}
+		},
+		
+		watch: {
+			css: {
+					files: 'sass/*.scss',
+					tasks: ['sass'],
+					options: {
+					  livereload: true,
+					  spawn: false,
+				  },
+			},
+			scripts:  { 
+        files: 'js/*.js', tasks: [ 'uglify' ] 
+      },
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks("grunt-sass");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.registerTask("default", ["watch", "jshint", "uglify", "sass"]);
+};
